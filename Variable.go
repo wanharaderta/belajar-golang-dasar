2b777cbf119c07e73da54017dde3039d4965c3d7
Wanhar <wanhardaengmaro@gmail.com>
2023-04-25 11:58:04 +0800

    adding variable

---
+++ Variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Wanhar"
+	fmt.Println(name)
+
+	name = "Daeng Maro"
+	fmt.Println(name)
+
+	var age = 10
+	fmt.Println(age)
+
+	country := "Afrikaans"
+	fmt.Println(country)
+
+	var (
+		firstName = "Wanhar"
+		lastName  = "Daeng Maro"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+}
