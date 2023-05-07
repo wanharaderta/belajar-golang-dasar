1ab2e2d3c364de0b7e5c2ff01cea672ec57e1b38
Wanhar <wanhardaengmaro@gmail.com>
2023-05-07 11:40:14 +0800

    function value

--- Function-value.go
+++ Function-value.go
@@ -2,11 +2,32 @@ package main
 
 import "fmt"
 
+// Type Declaration
+type Filter func(string) string
+
 func main() {
+	// Makes a function to be variable
 	goodBye := getGoodBye
 	fmt.Println(goodBye("Wanhar"))
+
+	// Makes a function to be parameter
+	sayHelloWithFilter("Wanhar", spamFilter)
+	sayHelloWithFilter("Anjing", spamFilter)
 }
 
 func getGoodBye(name string) string {
 	return "Good Bye " + name
+}
+
+func sayHelloWithFilter(name string, filter Filter) {
+	nameFiltered := filter(name)
+	fmt.Println("Hello", nameFiltered)
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing" {
+		return "..."
+	} else {
+		return name
+	}
 }
