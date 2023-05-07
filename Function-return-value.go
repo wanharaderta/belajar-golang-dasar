e31579ab7f470413b75c764c199755228531df93
Wanhar <wanhardaengmaro@gmail.com>
2023-05-07 11:08:44 +0800

    multiple value

--- Function-return-value.go
+++ Function-return-value.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	// single value
 	result := getHello("Wanhar")
 	fmt.Println(result)
+
+	// multi value
+	firstName, lastName := getFullName()
+	fmt.Println(firstName, lastName)
 }
 
+// Returning Single value
 func getHello(name string) string {
 	return "Hello," + name
+}
+
+// Returning Single value
+func getFullName() (string, string) {
+	return "Wanhar", "Daeng maro"
 }
